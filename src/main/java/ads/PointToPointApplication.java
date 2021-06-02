11f7ab9af137baf81bf509b1f9bc9276913c687d
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2021-06-01 23:25:45 -0300

    Hello World Springboot

---
+++ src/main/java/ads/PointToPointApplication.java
@@ -0,0 +1,13 @@
+package ads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointToPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointToPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ads/PointToPointApplicationTests.java
@@ -0,0 +1,13 @@
+package ads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointToPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
